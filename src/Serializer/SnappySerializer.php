347351ab128eae6ff7185cb8b7587de829f9c8e6
Jakub Vojacek <jakub@motv.eu>
2021-07-12 09:34:21 +0200

    snappy test

--- src/Serializer/SnappySerializer.php
+++ src/Serializer/SnappySerializer.php
@@ -10,7 +10,7 @@ final class SnappySerializer implements Serializer
 	 */
 	public function serialize($data, array &$meta): string
 	{
-		return @snappy_compress($data);
+		return @snappy_compress(json_encode($data));
 	}
 
 	/**
@@ -18,7 +18,7 @@ final class SnappySerializer implements Serializer
 	 */
 	public function unserialize(string $data, array $meta)
 	{
-		return @snappy_uncompress($data);
+		return json_decode(@snappy_uncompress($data));
 	}
 
 }
